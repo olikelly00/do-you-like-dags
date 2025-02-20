a90a2de8d98883415df80cf374ea24c419e067b4
Oli Kelly <olikelly@MASTUDENT-Oli-Kelly-NJ9KFPW96H.local>
2025-02-20 15:49:11 +0000

    batching

--- clean_posts_content_dag.py
+++ clean_posts_content_dag.py
@@ -8,12 +8,6 @@ import psycopg2
 from airflow.models import Variable
 import pandas as pd
 
-
-# STEP 1 -  SELECT old posts from source DB new posts from source DB
-# Transform: Filter out posts where body is empty - create new table of full-bodied posts?
-
-
-# Columns to extract: ID, title, body, owner_user_id, and creation_date
 """
 id   | owner_user_id | last_editor_user_id | post_type_id | 
 accepted_answer_id | score | parent_id | view_count | answer_count | 
@@ -21,8 +15,9 @@ comment_count | owner_display_name | last_editor_display_name | title |
 tags | content_license |  
 """
 
-
 BATCH_SIZE = 100000  
+TOTAL_ROWS = 57000000  
+NUM_BATCHES = TOTAL_ROWS // BATCH_SIZE
  
 default_args = {
     "owner": "airflow",
@@ -33,10 +28,6 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 
-# source_db_user = BaseHook.get_connection("source_db_user")
-# source_db_password = BaseHook.get_connection("source_db_password")
-# source_db_host = BaseHook.get_connection("source_db_host")
-
 source_db_user = Variable.get("source_db_user")
 source_db_password = Variable.get("source_db_password")
 source_db_host = Variable.get("source_db_host")
@@ -45,25 +36,10 @@ target_db_user = Variable.get("target_db_user")
 target_db_password = Variable.get("target_db_password")
 target_db_host = Variable.get("target_db_host")
 
-# target_db_user = BaseHook.get_connection("target_db_user")
-# target_db_password = BaseHook.get_connection("target_db_password")
-# target_db_host = BaseHook.get_connection("target_db_host")
 
-with DAG(
-    "extract_posts_sql_dag",
-    default_args=default_args,
-    description="A DAG to extract all posts from transactional DB",
-    schedule_interval=timedelta(days=1),
-    start_date=datetime(2023, 4, 28),
-    catchup=False,
-) as dag:
-    # """SELECT * FROM {{ task_instance.xcom_pull(task_ids='extract_posts_old_to_new', key='cleaned_posts') }}"""
-
-    # Extract cleaned post table from source DB
-    # Load new table into target db
-
-    def transfer_data(batch_number):
-        offset = batch_number * BATCH_SIZE
+def transfer_all_batches(**kwargs):
+        for batch_number in range(NUM_BATCHES):
+            offset = batch_number * BATCH_SIZE
 
         source_conn = psycopg2.connect(
             dbname="stackoverflow",
@@ -102,12 +78,6 @@ with DAG(
         target_cursor.execute(
             """CREATE TABLE IF NOT EXISTS cleaned_posts (id INTEGER PRIMARY KEY, title TEXT, body TEXT NOT NULL, owner_user_id INTEGER NOT NULL, creation_date DATE NOT NULL);"""
         )
-        # BATCH_SIZE = 10000
-
-        # while True:
-        #     rows = source_cursor.fetchmany(BATCH_SIZE)
-        #     if not rows:
-        #         break
 
         if rows:  
             target_cursor.executemany(
@@ -121,17 +91,25 @@ with DAG(
                         owner_user_id = EXCLUDED.owner_user_id,
                         creation_date = EXCLUDED.creation_date;
                     """, rows
-                )
+            )
 
         target_conn.commit()
         source_conn.close()
         target_conn.close()
 
 
+with DAG(
+    "extract_posts_sql_dag",
+    default_args=default_args,
+    description="A DAG to extract all posts from transactional DB",
+    schedule_interval=timedelta(days=1),
+    start_date=datetime(2023, 4, 28),
+    catchup=False,
+) as dag:
+
     transfer_task = PythonOperator(
-    task_id='transfer_data',
-    python_callable=transfer_data,
-    dag=dag,
+        task_id="transfer_all_batches",
+        python_callable=transfer_all_batches,  
     )
-        
-transfer_task
+    
+    transfer_task
